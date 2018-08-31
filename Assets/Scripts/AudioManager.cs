f57dcd434b4e89dc7c34d23c2b689d8b08129d15
Dan at Work <danmendelowitz@gmail.com>
2018-08-31 16:42:02 +0200

    Initial commit

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+	public static AudioManager instance;
+
+	public AudioMixer mixer;
+
+	public string masterVolume = "Master";
+	public string musicVolume = "Music";
+	public string ambientVolume = "Ambient";
+	public string sfxVolume = "SFX";
+
+	private void Awake()
+	{
+		if (instance==null)
+		{
+			instance = this;
+		}
+		else
+		{
+			Destroy(this.gameObject);
+		}
+	}
+
+	private void SetGroupVolume(string channelName, float volume)
+	{
+		float linearVolume = Mathf.Log(volume) * 20f;
+		mixer.SetFloat(channelName, linearVolume);
+	}
+
+	public void SetMasterVolume(float volume)
+	{
+		SetGroupVolume(masterVolume, volume);
+	}
+	public void SetMusicVolume(float volume)
+	{
+		SetGroupVolume(musicVolume, volume);
+	}
+	public void SetAmbientVolume(float volume)
+	{
+		SetGroupVolume(ambientVolume, volume);
+	}
+	public void SetSFXVolume(float volume)
+	{
+		SetGroupVolume(sfxVolume, volume);
+	}
+}
---
+++ Assets/Scripts/ClickToMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ClickToMove : MonoBehaviour
+{
+	public Camera cam;
+	public NavMeshAgent agent;
+	private Ray ray;
+	private RaycastHit hit;
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			ray = cam.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray, out hit))
+			{
+				agent.SetDestination(hit.point);
+			}
+		}
+	}
+}
